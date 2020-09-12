f46ff7f667efe6f1b5e98d4dd6645846d506d98d
Carlos Alejandro Ramos Hernández <carlos.ramos99@hotmail.com>
2020-09-11 19:44:32 -0500

    Version 1

---
+++ Models/Numeros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaMVC.Models
+{
+    public class Numeros
+    {
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+
+        public int Suma()
+        {
+            return N1 + N2;
+        }
+    }
+}
